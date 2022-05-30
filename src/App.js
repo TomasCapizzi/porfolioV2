422d5acd711c50159e020d2c70c55910a1cca41a
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-05-29 22:58:07 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './Styles/main.scss';
+
+import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/Footer';
+import LanguageContextProvider from './components/Languagecontext/LanguageContext';
+import Navbar from './components/Navbar';
+import React from 'react';
+import Routing from './components/Routing';
+
+function App() {
+
+/*  Heroku - github integration problem. Api turned down
+  async function projectsDB(){
+    const resp = await fetch('https://projects-api-tcapizzi.herokuapp.com/api/projects/all');
+    const res = await resp.json();
+    setProjectsAPI(res);
+  }
+  */
+
+
+}
+
+export default App;
