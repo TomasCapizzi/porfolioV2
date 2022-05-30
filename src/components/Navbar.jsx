dddf89aaf226135c4970b1ae32d68d80ef28eecf
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-05-29 23:01:25 -0300

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React,{useContext} from 'react';
+
+import {FaHome} from 'react-icons/fa'
+import {HiMenuAlt3} from 'react-icons/hi'
+import { LanguageContext } from './Languagecontext/LanguageContext';
+
+export default function Navbar(){
+
+   
+}
