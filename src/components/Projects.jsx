ec5b914e26eefa397251cae9d42cbac344318c4e
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-05-29 23:15:44 -0300

    Update Projects.jsx
    Show only first 3 projects, and manually next 3

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,9 +10,20 @@ import { projectsList } from '../projectsList';
 export default function Projects({projectsAPI}){
 
     //const [apiHandler, setApiHandler] = useState(false);
+    const [parameter, setParameter] = useState(3);
     const {english} = useContext(LanguageContext);
 
 
+    function showMoreProducts(){
+        setParameter(parameter + 3);      
+    }
+    function showLessProducts(){
+        setParameter(3)
+    }
+
+    useEffect(()=>{
+        //projectsAPI.length > 0 && setApiHandler(true);
+    },[parameter])
 
 
     return (
@@ -24,8 +35,23 @@ export default function Projects({projectsAPI}){
                     <svg className='svg-first' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#404040" fillOpacity="1" d="M0,128L720,192L1440,192L1440,320L720,320L0,320Z"></path></svg>
                     <article className='container-parent'>
                         <div className='container'>
+                            {
+                                projectsList.map(
+                                    item => <Project item={item} parameter={parameter} key={item.id} />
+                                )
+                            }
                         </div>
                     </article>
+                    {parameter <= projectsList.length ? 
+                        <div className='btnProj'>
+                            <AiFillPlusCircle onClick={showMoreProducts} className='btnProj'/>
+                            <p>{english ? 'See more' : 'Mostrar más'}</p>
+                        </div>
+                    :   <div className='btnProj'>
+                            <a href="#projects"><FaMinusCircle onClick={showLessProducts}/></a>
+                            <p>{english ? 'Show less' : 'Mostrar menos'}</p>
+                        </div> 
+                        }
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#404040" fillOpacity="1" d="M0,128L720,192L1440,192L1440,0L720,0L0,0Z"></path></svg>
                 </div>
                 :
