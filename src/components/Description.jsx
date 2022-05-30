1f88bb2cd39d4131192ad550466c459c6b78acc2
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-05-29 22:59:34 -0300

    Create Description.jsx

---
+++ src/components/Description.jsx
@@ -0,0 +1,23 @@
+import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai';
+import React,{useContext, useRef, useState} from 'react';
+
+import { LanguageContext } from './Languagecontext/LanguageContext';
+
+export default function Description(){
+
+    const {english} = useContext(LanguageContext);
+
+    return(
+        <article className='description' id='aboutme' >
+            <img src="./img/115.jpg" alt="profile" />
+            <article className='info'>
+                <h1 id='about-me'>{english ? `Hi, I'm Tomas Capizzi !` : 'Hola, soy Tomás Capizzi'}</h1>
+                <h2>FRONT END DEVELOPER</h2>
+                <ul>
+                    <li><a href="https://www.linkedin.com/in/tomas-capizzi/" target="_blank" rel="noreferrer" alt="Linkedin Tomas Capizzi"><AiFillLinkedin/></a></li>
+                    <li><a href="https://github.com/TomasCapizzi" target="_blank" rel="noreferrer"><AiFillGithub/></a></li>
+                </ul>
+            </article>
+        </article>
+    )
+}
