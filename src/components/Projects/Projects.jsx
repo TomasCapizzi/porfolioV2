16e0b95e7af5ee70768261a5214891b87783dd16
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-04 18:45:24 -0300

    Use new projects API

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,13 +6,15 @@ import { LanguageContext } from '../../context/LanguageContext';
 import Project from './Project';
 import Spinner from '../Spinner';
 import { projectsList } from '../../projectsList';
+import useFetchProjects from '../../hooks/useFetchProjects';
 
 export default function Projects({projectsAPI}){
 
-    //const [apiHandler, setApiHandler] = useState(false);
+    const [projectsHandler, setProjectsHandler] = useState(false);
     const [parameter, setParameter] = useState(3);
     const {english} = useContext(LanguageContext);
 
+    const {projects, getProyects} = useFetchProjects()
 
     function showMoreProducts(){
         setParameter(parameter + 3);      
@@ -20,29 +22,31 @@ export default function Projects({projectsAPI}){
     function showLessProducts(){
         setParameter(3)
     }
+    
 
     useEffect(()=>{
         //projectsAPI.length > 0 && setApiHandler(true);
-    },[parameter])
+        getProyects()
+    },[])
 
 
     return (
         <div className='projects'>
             <h3 id='projects'>{english ? 'Projects' : 'Proyectos'}</h3>
             {
-                projectsList ? 
+                projects ? 
                 <div className='projects-container'>
                     <svg className='svg-first' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#404040" fillOpacity="1" d="M0,128L720,192L1440,192L1440,320L720,320L0,320Z"></path></svg>
                     <article className='container-parent'>
                         <div className='container'>
                             {
-                                projectsList.map(
+                                projects.map(
                                     item => <Project item={item} parameter={parameter} key={item.id} />
                                 )
                             }
                         </div>
                     </article>
-                    {parameter <= projectsList.length ? 
+                    {parameter <= projects.length ? 
                         <div className='btnProj'>
                             <AiFillPlusCircle onClick={showMoreProducts} className='btnProj'/>
                             <p>{english ? 'See more' : 'Mostrar más'}</p>
