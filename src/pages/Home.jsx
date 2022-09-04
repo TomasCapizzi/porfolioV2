b3f40ec466e68d75ed832b3cef67aa57b448be2e
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-04 18:46:25 -0300

    Add new component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
-import Contact from '../components/Contact';
+import React,{useEffect} from 'react';
+
+import Contact from '../components/Contact/Contact';
 import Description from '../components/Description';
 import PersonalInfo from '../components/PersonalInfo';
+import PortfolioDescription from '../components/PortfolioDescription';
 import Projects from '../components/Projects/Projects';
-import React from 'react';
 import Stack from '../components/Stack';
 
-function Home() {
+function Home() {  
   return (
     <section className='home'>
         <Description />
         <Stack/>
         <Projects/>
         <PersonalInfo/>
+        <PortfolioDescription/>
         <Contact/>
     </section>
   )
