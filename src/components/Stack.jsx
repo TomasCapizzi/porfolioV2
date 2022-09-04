d0a23d16bdec0810efcc71da01d8ba9a86767933
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-04 18:45:47 -0300

    Atomic design

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -1,45 +1,14 @@
-import {AiFillGithub, AiFillHtml5} from 'react-icons/ai';
-import {FaAws, FaCss3Alt, FaGitAlt, FaNodeJs, FaReact, FaSass} from 'react-icons/fa';
-import {IoBrush, IoCodeSlashSharp, IoSettings} from 'react-icons/io5';
-import {SiAdobeaftereffects, SiAdobeillustrator, SiAdobephotoshop, SiAdobepremiere, SiFirebase, SiJavascript, SiMongodb, SiRedux} from 'react-icons/si';
-
+import CodeStack from './Stack/CodeStack';
+import DesignStack from './Stack/DesignStack';
 import React from 'react';
+import ToolsStack from './Stack/ToolsStack';
 
 function Stack() {
   return (
     <article className='stack'>    
-          <div className='list'>
-            <span><IoCodeSlashSharp/></span>
-            <ul>
-              <li><AiFillHtml5/><p>Html</p></li>
-              <li><FaCss3Alt/><p>Css</p></li>
-              <li><FaSass/><p>Sass</p></li>
-              <li><SiJavascript/><p>Javascript</p></li>
-              <li><FaNodeJs/><p>Node Js</p></li>
-              <li><FaReact/><p>React</p></li>
-              <li><FaReact/><p>React Native</p></li>
-            </ul>
-          </div>
-          <div className='list'>
-            <span><IoSettings/></span>
-            <ul>
-              <li><SiFirebase/><p>Firebase</p></li>
-              <li><SiMongodb/><p>Mongo DB</p></li>
-              <li><FaGitAlt/><p>Git</p></li>
-              <li><AiFillGithub/><p>Github</p></li>
-              <li><SiRedux/><p>Redux</p></li>
-              <li><FaAws/><p>AWS</p></li>
-            </ul>
-          </div>
-          <div className='list'>
-            <span><IoBrush/></span>
-            <ul>
-              <li><SiAdobepremiere/><p>Premiere</p></li>
-              <li><SiAdobeaftereffects/><p>After Effects</p></li>
-              <li><SiAdobeillustrator/><p>Illustrator</p></li>
-              <li><SiAdobephotoshop/><p>Photoshop</p></li>
-            </ul>
-          </div>
+      <CodeStack/>
+      <ToolsStack/> 
+      <DesignStack/>
     </article>
   )
 }
