9a855b4087b0e1b52829c86e955839feae407258
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-04 18:46:14 -0300

    custom hook for fetch api data

---
+++ src/hooks/useFetchProjects.js
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+
+function useFetchProjects() {
+    const [projects, setProjects] = useState();
+    const url = 'https://projects-api-tcapizzi-sql.herokuapp.com/api/proyectos';
+
+    async function getProyects(){
+      const response = await fetch(url);
+      const res = await response.json()
+      return setProjects(res.data)
+    }
+
+
+   // console.log(res.data);
+  return {projects, getProyects}
+}
+
+export default useFetchProjects;
