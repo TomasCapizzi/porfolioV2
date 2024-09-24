5be71ddfb06747d07744551f55d827acba32ab37
TomasCapizzi <tomas.capizzi@hotmail.com>
2024-09-24 18:29:25 -0300

    test background style

---
+++ src/components/Background.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Background() {
+  return (
+    <div class="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+  )
+}
