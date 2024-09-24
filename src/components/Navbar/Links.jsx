6b7b0d4b303014cd4fb53bb158eb532a64e0baae
TomasCapizzi <tomas.capizzi@hotmail.com>
2024-09-24 18:31:32 -0300

    Update Links.jsx

--- src/components/Navbar/Links.jsx
+++ src/components/Navbar/Links.jsx
@@ -1,11 +1,9 @@
-import {FaHome} from 'react-icons/fa'
 import React from 'react'
 
 function Links({english}) {
   return (
     <ul>
-        <a href='/' rel="noreferrer"><li><FaHome/></li></a>
-        <a href="/#info-title" rel="noreferrer"><li>{english ? 'About Me' : '¿Quién Soy?'}</li></a>
+        <a href="/#about-me" rel="noreferrer"><li>{english ? 'About Me' : '¿Quién Soy?'}</li></a>
         <a href="/#projects" rel="noreferrer"><li>{english ? 'Projects' : 'Proyectos'}</li></a>
         <a href="/#contact" rel="noreferrer"><li>{english ? 'Contact' : 'Contacto'}</li></a>
     </ul>
